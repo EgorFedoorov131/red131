8b3af5337fd2003b555326a729fa9db4a29e0800
Egor.Fyodorov <1426200@mail.ru>
2020-02-13 17:28:34 +0500

    Enum234

---
+++ VIN_LIB/VIN_LIB/VIN_LIB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace VIN_LIB
+{
+    public class VIN_LIB
+    {
+    bool CheckVIN(string vin)
+        {
+            Regex reg = new Regex(@"[A-H,J-N,P,R-Z,\d]{17}");
+            if (!reg.IsMatch(vin)) return false;
+
+            return true;
+        }
+    string GetVINCountry(string vin)
+        {
+            return null;
+        }
+    int GetTransportYear(string vin)
+        {
+            return 0;
+        }
+    }
+}
