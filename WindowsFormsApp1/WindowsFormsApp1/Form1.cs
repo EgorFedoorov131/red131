4f7de2ced72f1f7f23972d4f8f8ca6b3e7724fa5
Egor.Fyodorov <1426200@mail.ru>
2020-02-18 16:40:10 +0500

    form

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -7,14 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
-namespace WindowsFormsApp1
+namespace gibdd
 {
     public partial class Form1 : Form
     {
+        SqlConnection con = new SqlConnection("Data Source=303-16\\SQLServer; Initial Catalog=gibdd; Integrated Security=true;");
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlComman con = new SqlCommand($"Select login FROM usrers where login = '{textBox1.Text}' and pass = '{textBox2.Text}'", con);
+            SqlDataReader dr = com.ExecuteReader();
+            if (dr.HasRows)
+                MessageBox.Show("good");
+            else
+            {
+
+            }
+            string login = textBox1.Text;
+            string password = textBox2.Text;
         }
     }
 }
