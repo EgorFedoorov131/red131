cef25654df56b0b1eb209ef1ccd09192e40f4202
Egor.Fyodorov <1426200@mail.ru>
2020-02-20 16:15:10 +0500

    123

--- gibdd/gibdd/Form2.cs
+++ gibdd/gibdd/Form2.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace gibdd
 {
@@ -15,6 +16,11 @@ namespace gibdd
         public Form2()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
